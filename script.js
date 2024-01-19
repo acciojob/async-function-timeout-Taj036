8f49da0a29cfb50594400a4d3c85160de32a51c1
Taj036 <121137573+Taj036@users.noreply.github.com>
2024-01-19 21:50:51 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+document.getElementById('btn').addEventListener('click', async function () {
+  const textInput = document.getElementById('text').value;
+  const delayInput = document.getElementById('delay').value;
+  const delayInMillis = parseInt(delayInput) * 1000;
+  const result = await delayAndDisplay(textInput, delayInMillis);
+  document.getElementById('output').innerText = result;
+});
+async function delayAndDisplay(message, delay) {
+  const delayPromise = new Promise(resolve => {
+    setTimeout(() => {
+      resolve(message);
+    }, delay);
+  });
+  return await delayPromise;
+}
